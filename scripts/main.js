db952fb522f7fdc894f45f9d44a28210a73bfa1e
Joshwelyt <71865382+Joshwelyt@users.noreply.github.com>
2021-03-04 20:15:18 +0800

    Create main.js
    Sector via js might or might not work high chance of work cause yes pro coder gam ing

---
+++ scripts/main.js
@@ -0,0 +1,6 @@
+var silverCrags = extend(SectorPreset, "silver-crags", Planets.serpulo, 206, {
+    localizedName: "Silver Crags",
+    difficulty: 5,
+    alwaysUnlocked: true
+});
+//just rename stuff if u know js u know what to do
